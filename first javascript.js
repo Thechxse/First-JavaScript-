266acf13e8b92531fde26109b1fcf29283efe6ab
Thechxse <105224691+Thechxse@users.noreply.github.com>
2022-06-24 07:12:10 +0100

    Add files via upload

---
+++ first javascript.js
@@ -0,0 +1,4 @@
+// Writing a JS code that prints out my name, height, and country on the screen.
+console.log('Daniel Boluwatife Aaron ')
+console.log('Height: 178cm')
+console.log('Country: Nigeria')
